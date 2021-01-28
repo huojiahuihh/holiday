9685057ac1234e0acd32126c4cf318433dd445d0
huojiahuihh <huojiahuihh@163.com>
2021-01-28 15:47:39 +0800

    规格参数

---
+++ mingrui-shop-service-api/mingrui-shop-service-api-xxx/src/main/java/com/baidu/shop/service/SpecificationService.java
@@ -0,0 +1,57 @@
+package com.baidu.shop.service;
+
+import com.baidu.shop.base.Result;
+import com.baidu.shop.dto.SpecGroupDTO;
+import com.baidu.shop.dto.SpecParamDTO;
+import com.baidu.shop.entity.SpecGroupEntity;
+import com.baidu.shop.entity.SpecParamEntity;
+import com.baidu.shop.validate.group.MingruiOperation;
+import com.google.gson.JsonObject;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Api(tags = "规格参数")
+public interface SpecificationService {
+
+    @ApiOperation(value = "通过条件查询规格组")
+    @GetMapping(value = "specgroup/getSpecGroupInfo")
+    Result<List<SpecGroupEntity>> getSpecGroupInfo(SpecGroupDTO specGroupDTO);
+
+    @ApiOperation(value = "新增规格组")
+    @PostMapping(value = "specgroup/save")
+    Result<JsonObject> saveGroupInfo(@Validated({MingruiOperation.Add.class})@RequestBody SpecGroupDTO specGroupDTO);
+
+    @ApiOperation(value = "修改规格组")
+    @PutMapping(value = "specgroup/save")
+    Result<JsonObject> editGroupInfo(@Validated({MingruiOperation.Update.class})@RequestBody SpecGroupDTO specGroupDTO);
+
+    @ApiOperation(value = "删除规格组")
+    @DeleteMapping(value = "specgroup/delete/{id}")
+    Result<JsonObject> deleteGroupId(@NotNull @PathVariable Integer id);
+
+
+    //查询规格参数
+    @ApiOperation(value = "查询规格参数")
+    @GetMapping(value = "specParam/getSpecParamInfo")
+    Result<List<SpecParamEntity>> getSpecParamInfo(SpecParamDTO specParamDTO);
+
+    //新增规格参数
+    @ApiOperation(value = "新增规格参数")
+    @PostMapping(value = "specParam/save")
+    Result<JsonObject> saveParamInfo(@Validated({MingruiOperation.Add.class})@RequestBody SpecParamDTO specParamDTO);
+
+    //修改规格参数
+    @ApiOperation(value = "修改规格参数")
+    @PutMapping(value = "specParam/save")
+    Result<JsonObject> editParamInfo(@Validated({MingruiOperation.Update.class})@RequestBody SpecParamDTO specParamDTO);
+
+    //删除规格参数
+    @ApiOperation(value="删除规格参数")
+    @DeleteMapping(value = "specParam/delete")
+    Result<JsonObject> deleteParamId(@NotNull Integer id);
+}
---
+++ mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/SpecificationServiceImpl.java
@@ -0,0 +1,103 @@
+package com.baidu.shop.service.impl;
+
+import com.baidu.shop.base.BaseApiService;
+import com.baidu.shop.base.Result;
+import com.baidu.shop.dto.SpecGroupDTO;
+import com.baidu.shop.dto.SpecParamDTO;
+import com.baidu.shop.entity.SpecGroupEntity;
+import com.baidu.shop.entity.SpecParamEntity;
+import com.baidu.shop.mapper.SpecGroupMapper;
+import com.baidu.shop.mapper.SpecParamMapper;
+import com.baidu.shop.service.SpecificationService;
+import com.baidu.shop.utils.BaiduBeanUtil;
+import com.baidu.shop.utils.ObjectUtil;
+import com.google.gson.JsonObject;
+import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
+
+import javax.annotation.Resource;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+public class SpecificationServiceImpl extends BaseApiService implements SpecificationService {
+
+    @Resource
+    private SpecGroupMapper specGroupMapper;
+
+    @Resource
+    private SpecParamMapper specParamMapper;
+
+    @Override
+    public Result<List<SpecGroupEntity>> getSpecGroupInfo(SpecGroupDTO specGroupDTO) {
+
+        Example example = new Example(SpecGroupEntity.class);
+
+        example.createCriteria().andEqualTo("cid", BaiduBeanUtil.copyProperties(specGroupDTO,SpecGroupEntity.class).getCId());
+
+        List<SpecGroupEntity> specGroupEntities = specGroupMapper.selectByExample(example);
+        return this.setResultSuccess(specGroupEntities);
+    }
+
+    //新增
+    @Override
+    @Transactional
+    public Result<JsonObject> saveGroupInfo(SpecGroupDTO specGroupDTO) {
+        specGroupMapper.insertSelective(BaiduBeanUtil.copyProperties(specGroupDTO,SpecGroupEntity.class));
+        return this.setResultSuccess();
+    }
+
+    //修改
+    @Override
+    @Transactional
+    public Result<JsonObject> editGroupInfo(SpecGroupDTO specGroupDTO) {
+        specGroupMapper.updateByPrimaryKeySelective(BaiduBeanUtil.copyProperties(specGroupDTO,SpecGroupEntity.class));
+        return this.setResultSuccess();
+    }
+
+    //删除
+    @Override
+    public Result<JsonObject> deleteGroupId(@NotNull Integer id) {
+
+        //删除规格组之前先判断一下当前规格组下是有有其他规格参数
+        Example example = new Example(SpecParamEntity.class);
+        example.createCriteria().andEqualTo("groupId",id);
+        List<SpecParamEntity> specParamEntities = specParamMapper.selectByExample(example);
+        if (specParamEntities.size()>0) return this.setResultError("当前规格组下有其他规格,所以不能删除");
+        specGroupMapper.deleteByPrimaryKey(id);
+        return this.setResultSuccess();
+    }
+
+    @Override
+    public Result<List<SpecParamEntity>> getSpecParamInfo(SpecParamDTO specParamDTO) {
+
+        SpecParamEntity specParamEntity = BaiduBeanUtil.copyProperties(specParamDTO, SpecParamEntity.class);
+        Example example = new Example(SpecGroupEntity.class);
+        Example.Criteria criteria = example.createCriteria();
+
+        if (ObjectUtil.isNotNull(specParamEntity.getGroupId())) criteria.andEqualTo("groupId",specParamEntity.getGroupId());
+
+        if (ObjectUtil.isNotNull(specParamEntity.getCid()))
+            criteria.andEqualTo("cid",specParamEntity.getCid());
+
+        List<SpecParamEntity> specParamEntities = specParamMapper.selectByExample(example);
+        return this.setResultSuccess(specParamEntities);
+    }
+
+    @Override
+    public Result<JsonObject> saveParamInfo(SpecParamDTO specParamDTO) {
+        specParamMapper.insertSelective(BaiduBeanUtil.copyProperties(specParamDTO,SpecParamEntity.class));
+        return this.setResultSuccess();
+    }
+
+    @Override
+    public Result<JsonObject> editParamInfo(SpecParamDTO specParamDTO) {
+        specParamMapper.updateByPrimaryKeySelective(BaiduBeanUtil.copyProperties(specParamDTO,SpecParamEntity.class));
+        return this.setResultSuccess();
+    }
+
+    @Override
+    public Result<JsonObject> deleteParamId(@NotNull Integer id) {
+        specParamMapper.deleteByPrimaryKey(id);
+        return this.setResultSuccess();
+    }
+}
